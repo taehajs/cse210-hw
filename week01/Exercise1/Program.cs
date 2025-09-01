000542fcb642cda098ecd58cf7333cb6fe29297f
Kim JiSeung <taehajs@naver.com>
2025-09-01 21:09:59 +0900

    11

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("What is your frist name?");
+        string frist = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string last = Console.ReadLine();
+
+
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
     }
 }
