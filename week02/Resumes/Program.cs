d429331f92074aa194e8a448822883a79a499816
Kim JiSeung <taehajs@naver.com>
2025-09-08 19:01:52 +0900

    R

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -16,7 +16,7 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        Resume myResume = new resume();
+        Resume myResume = new Resume();
         myResume._name = "Allison Rose";
 
         myResume._jobs.Add(job1);
